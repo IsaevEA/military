8a231e50f9f48daadb8f4ed51b33bc957bea2adb
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-07-19 20:21:52 +0600

    просьба ментора

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
         Soldier soldier4 = new Soldier("Max", "Soldier", 22, backpack,  new Weapon("Зеленый", 5) );
 
         Officers officers = new Officers("Max", "Soldier", 22, backpack, map );
+        Officers officers1 = new Officers("Max", "Soldier", 22, backpack, map );
+        Officers officers2= new Officers("Max", "Soldier", 22, backpack, map );
+        Officers officers3= new Officers("Max", "Soldier", 22, backpack, map );
 
         Squad squad = new Squad();
         squad.add(soldier1);
